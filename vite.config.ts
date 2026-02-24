8662ccc7b52082f33a1d99472f921d48f28e0eec
Matt Pryer <81967541+mwpryer@users.noreply.github.com>
2026-02-24 19:51:41 +0000

    feat: init registry

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? "/ui/" : "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
